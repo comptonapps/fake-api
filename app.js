9e12db4432ea58bec835646cbd7afc7b2c2ca495
Jonathan Compton <jonathankcompton@gmail.com>
2022-06-14 18:02:14 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const PORT = process.env.PORT || 4455;
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+    return res.json({foo: true})
+});
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+});
